d9104c107ea3b663f93e146128582c7905530e5c
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-13 23:33:28 +0300

    Create a database and connect Postgres to the application

--- photo/views.py
+++ photo/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request,'all/pics.html')
+
+def search(request):
+  return render(request, 'all/search.html')
