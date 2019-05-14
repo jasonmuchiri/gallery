1ada54e243c073b0fe352c8b381823b446484531
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-14 10:21:15 +0300

    Add models to my application

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  category = models.TextField()
+
+class Location(models.Model):
+  location = models.TextField()  
+
+class Image(models.Model):
+  image = models.ImageField(upload_to = 'images/')
+  image_name = models.CharField(max_length=60)
+  image_description = models.TextField()
+  pub_date = models.DateTimeField(auto_now_add=True)
+  category = models.ManyToManyField(Category)
+  location = models.ForeignKey(Location)
