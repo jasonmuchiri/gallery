855758868d57ba96f148b7e9fb70feb3b9c19b65
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-14 10:54:07 +0300

    Update our views.py file

--- photo/views.py
+++ photo/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, render_to_response, redirect
+import datetime as dt
+from django.http import HttpResponse, Http404
+from .models import Image,Location, Category
 
 # Create your views here.
 def welcome(request):
-    return render(request,'all/pics.html')
+    pics = Image.all_images()
+    return render(request,'all/pics.html',{'pics':pics})
+
+def filter_usa(request):
+  pics = Image.filter_images('USA')
+  return render(request, 'all/search.html', {'pics':pics})
+
+def filter_germany(request):
+  pics = Image.filter_images('Germany')
+  return render(request, 'all/search.html', {'pics':pics})
+
+def filter_italy(request):
+  pics = Image.filter_images('Italy')
+  return render(request, 'all/search.html', {'pics':pics})
+
+def filter_brazil(request):
+  pics = Image.filter_images('Brazil')
+  return render(request, 'all/search.html', {'pics':pics})
+
+def filter_kenya(request):
+  pics = Image.filter_images('Kenya')
+  return render(request, 'all/search.html', {'pics':pics})
 
 def search(request):
-  return render(request, 'all/search.html')
+  if 'location' in request.GET and request.GET['location']:
+    search_term = request.GET.get('location')
+    searched_pics = Image.search_image(search_term)
+    return render(request, 'all/search.html', {'pics':searched_pics})
+
+  else:
+    return render(request, 'all/search.html')
+
