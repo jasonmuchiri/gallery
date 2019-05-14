ac1829064e6809e5ec2e8245a03223f3362e313e
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-14 11:16:28 +0300

    Add an admin page and add models to the page

--- photo/admin.py
+++ photo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Image, Category, Location
 
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+  filter_horizontal=('Category')
+  filter_horizontal=('Location')
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
--- photo/urls.py
+++ photo/urls.py
@@ -5,6 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^filter-usa$', views.filter_usa, name='filter-usa'),
+    url('^filter-germany$', views.filter_germany, name='filter-germany'),
+    url('^filter-italy$', views.filter_italy, name='filter-italy'),
+    url('^filter-brazil$', views.filter_brazil, name='filter-brazil'),
+    url('^filter-kenya$', views.filter_kenya, name='filter-kenya'),
     url('^&',views.search, name='search')
 ]
 
