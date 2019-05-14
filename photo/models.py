1b48d49c5fe4540496acf59417e2f317d7c2b836
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-14 10:26:04 +0300

    Make changes made in the models.py file

--- photo/models.py
+++ photo/models.py
@@ -4,8 +4,27 @@ from django.db import models
 class Category(models.Model):
   category = models.TextField()
 
+  def save_category(self):
+    self.save()
+
+  def del_category(self):
+    self.delete()
+
+  def __str__(self):
+    return self.category
+
+
 class Location(models.Model):
-  location = models.TextField()  
+  location = models.TextField()
+
+  def save_location(self):
+    self.save()
+
+  def del_location(self):
+    self.delete()
+
+  def __str__(self):
+    return self.location  
 
 class Image(models.Model):
   image = models.ImageField(upload_to = 'images/')
@@ -14,3 +33,35 @@ class Image(models.Model):
   pub_date = models.DateTimeField(auto_now_add=True)
   category = models.ManyToManyField(Category)
   location = models.ForeignKey(Location)
+
+  def save_image(self):
+    self.save()
+
+  def del_image(self):
+    self.delete()
+
+  def get_image(self, id):
+    image = Image.objects.get(pk=id)
+    return image
+
+  def update_image(self, id, updated_name):
+    image = Image.objects.filter(pk=id).update(image_name=updated_name)
+    return image.save_image()
+
+  @classmethod
+  def all_images(cls):
+    images = cls.objects.all()
+    return images
+
+  @classmethod
+  def search_image(cls, search):
+    image = cls.objects.filter(category__category__icontains=search)
+    return image
+
+  @classmethod
+  def filter_images(cls, place):
+    images = cls.objects.filter(location__location=place)
+    return images
+
+  def __str__(self):
+    return self.image.url
